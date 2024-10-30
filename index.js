0e5e75ef27b6dd23e5ee832ec9b3844a4b51a352
ROMEK XD <mdfaiyazraj786@gmail.com>
2024-10-30 22:18:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -95,8 +95,8 @@ async function start() {
                 }
             } else if (connection === 'open') {
                 if (initialConnection) {
-                    console.log(chalk.green("😃ROMEK-XD-CONNECTED Successful️✅ JION FOR MORE UPDATE🥏"));
-                    Matrix.sendMessage(Matrix.user.id, { text: `😃ROMEK-XD-CONNECTED Successful️✅ JION FOR MORE UPDATE🥏` });
+                    console.log(chalk.green("😃ROMEK-XD-CONNECTED Successful️✅ JOIN FOR MORE UPDATE🥏 https://whatsapp.com/channel/0029VakaPzeD38CV78dbGf0e"));
+                    Matrix.sendMessage(Matrix.user.id, { text: `😃ROMEK-XD-CONNECTED Successful️✅ JOIN FOR MORE UPDATE🥏 https://whatsapp.com/channel/0029VakaPzeD38CV78dbGf0e` });
                     initialConnection = false;
                 } else {
                     console.log(chalk.blue("♻️ Connection reestablished after restart."));
